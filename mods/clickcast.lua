ee3ec95d2a76ba3b9074889ba1d23489396b1298
wardz <thomaszrx@gmail.com>
2017-03-14 08:18:23 +0100

    add pfUI clickcast support

---
+++ mods/clickcast.lua
@@ -0,0 +1,31 @@
+local Loader = getfenv(0).Focus_Loader
+
+-- luna clickcasting
+Loader:Register("LunaUnitFrames", function(Focus)
+    local orig_lufmo = SlashCmdList.LUFMO
+
+    SlashCmdList.LUFMO = function(arg1, arg2)
+        local frame = GetMouseFocus()
+
+        if arg1 and strfind(frame:GetName() or "", "FocusFrame") then
+            Focus:Call(CastSpellByName, arg1)
+        else
+            orig_lufmo(arg1, arg2)
+        end
+    end
+end)
+
+-- pfUI clickcast
+Loader:Register("pfUI", function(Focus)
+    local orig_pfcast = SlashCmdList.PFCAST
+
+    SlashCmdList.PFCAST = function(arg1, arg2)
+        local frame = GetMouseFocus()
+
+        if arg1 and strfind(frame:GetName() or "", "FocusFrame") then
+            Focus:Call(CastSpellByName, arg1)
+        else
+            orig_pfcast(arg1, arg2)
+        end
+    end
+end)
